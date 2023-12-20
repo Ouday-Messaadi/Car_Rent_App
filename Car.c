fe247ca843a74cf5f8914be102dba3a0728fdb65
eyaziri <149670348+eyaziri@users.noreply.github.com>
2023-12-20 16:23:50 +0100

    Update Car.c

--- Car.c
+++ Car.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "car.h"
 
@@ -13,23 +15,50 @@ void AddCar (car* car1,Account* acc)
     car1->accountClient=NULL;
 }
 //*************************************************************//
-void postcar(car*car1,Account* acc)
+
+void postcar(car*car1,Account acc)
+{
+    printf("The car's color is  : %s ",car1.CarColor);
+    printf("The car's model is  : %s ",car1.CarModel);
+    printf("The car's price per hour is : %f ",car1.CarPriceRentPerHour);
+    Post_Account (acc);
+
+}
+
+
+//*************************************************************//
+
+void searchCar(car*car1)
 {
     char ch[50];
     char ch1[50];
     float price ;
-    do{
     printf("Enter the car's color you want : ");
     gets(ch);
-    }while(strcmp(ch,car1.CarColor)!=0);
-    do{
+    if ((strcmp(ch,car1.CarColor)!=0))
+    {
+        printf("The car's color is not available, Please choose another color");
+        gets(ch);
+    }
+
+    else
+    {
         printf("Enter the car's model you want :");
         gets(ch1);
-    }while(strcmp(ch1,car1.CarModel)!=0);
-    do{
-        printf("Enter the car's price you want per hour :");
-        scanf("%f",price);
-    }while(price!=car1.CarPriceRentPerHour);
+        if (strcmp(ch1,car1.CarModel)!=0)
+        {
+            printf("The car's model is not available, Please choose another model");
+            gets(ch1);
+        }
+        else
+        {
+            printf("Enter the car's price you want per hour :");
+            scanf("%f",price);
+            if (price!=car1.CarPriceRentPerHour)
+            {
+                printf("This car's price is not available, Please try another price");
+                scanf("%f",price);
+            }
 
-}
+        }
 
