b55bd5cf9aa53e20e7bf13b5efe4d678a517a237
eyaziri <149670348+eyaziri@users.noreply.github.com>
2023-12-20 16:05:11 +0100

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,6 +1,16 @@
 #ifndef CAR_H_INCLUDED
 #define CAR_H_INCLUDED
+#include "Account.h"
+typedef struct
+{
+    char CarColor [50] ;
+    char CarModel [50];
+    float CarPriceRentPerHour ;
+    Account* accountOwner ;
+    Account* accountClient ;
+} car;
 
 
-
+void AddCar (car*,Account*);
+void postcar(car*,Account*);
 #endif // CAR_H_INCLUDED
